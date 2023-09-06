16e1db77bbd3a8763cb1b9ffb8801f00c7c342a9
Julia <ibralenko6724809@gmail.com>
2023-09-06 19:27:51 +0300

    add  style

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,18 @@ import data from './data';
 function App() {
   return (
     <div className="App">
-      <div className="card">
+      <div className="card-list">
         {data.map((hero) => {
           return (
-            <div >
+            <div className="card">
               <h1 className="card__title">{hero.name}</h1>
               <h3 className="card__info">Вселенная: {hero.universe}</h3>
               <h3 className="card__info">Альтер эго: {hero.alterego}</h3>
               <h3 className="card__info">Род деятельности: {hero.occupation}</h3>
               <h3 className="card__info">Друзья: {hero.friends}</h3>
               <h3 className="card__info">Суперсила: {hero.superpowers}</h3>
-              <h3 className="card__info">Информация: {hero.info}</h3>
               <img className="card__image" src={hero.url} alt={hero.name}></img>
-              <hr />
+              <p className="card__text">Информация: {hero.info}</p>
             </div>
           );
         })}
